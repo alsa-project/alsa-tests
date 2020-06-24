1b087a366378daff9630735be879a9105fdda819
Jaroslav Kysela <perex@perex.cz>
2020-06-24 12:55:23 +0200

    ucm-validator: print DefineRegex to log
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- python/ucm-validator/ucmlib.py
+++ python/ucm-validator/ucmlib.py
@@ -1037,10 +1037,12 @@ class Ucm:
             m = re.match('.*' + reg, string)
             if not m:
                 continue
+            self.log(1, "DefineRegex.%s = %s", node.id, m.group(0))
             self.var[node.id] = m.group(0)
             g = m.groups()
             idx = 1
             for a in g:
+                self.log(1, "DefineRegex.%s%s = %s", node.id, str(idx), a)
                 self.var[node.id + str(idx)] = a
                 idx += 1
             ret = True
