88ab572323804516715601b2207f496e85f41e94
Jaroslav Kysela <perex@perex.cz>
2020-07-06 17:05:17 +0200

    ucm-validator: hda - suppress errors for HDMI4,HDMI5,HDMI6
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- python/ucm-validator/configs/HDA/dummy.conf.py
+++ python/ucm-validator/configs/HDA/dummy.conf.py
@@ -3,7 +3,11 @@
 #
 
 SUPPRESS_IF = {
-  "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'acp'.'Condition' - True": 1
+  "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'acp'.'Condition' - True": 1,
+  "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'hdmi'.'True'.'Include'.'hdmi'.'If'.'hdmi4'.'True'.'Include'.'hdmi4'.'If'.'hdmi'.'Condition' - True": 1,
+  "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'hdmi'.'True'.'Include'.'hdmi'.'If'.'hdmi5'.'True'.'Include'.'hdmi5'.'If'.'hdmi'.'Condition' - True": 1,
+  "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'hdmi'.'True'.'Include'.'hdmi'.'If'.'hdmi6'.'True'.'Include'.'hdmi5'.'If'.'hdmi'.'Condition' - True": 1,
+
 }
 
 def generate_config(topdir):
