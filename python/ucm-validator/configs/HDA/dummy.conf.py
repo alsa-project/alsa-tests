ebb0be8d473e33169ed24fd24971a8cfe8362675
Jaroslav Kysela <perex@perex.cz>
2020-09-07 17:37:11 +0200

    ucm-validator: HDA - suppress the ACP warning for the latest ucm2
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- python/ucm-validator/configs/HDA/dummy.conf.py
+++ python/ucm-validator/configs/HDA/dummy.conf.py
@@ -4,6 +4,7 @@
 
 SUPPRESS_IF = {
   "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'acp'.'Condition' - True": 1,
+  "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'analog'.'True'.'If'.'acp'.'Condition' - True": 1,
   "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'hdmi'.'True'.'Include'.'hdmi'.'If'.'hdmi4'.'True'.'Include'.'hdmi4'.'If'.'hdmi'.'Condition' - True": 1,
   "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'hdmi'.'True'.'Include'.'hdmi'.'If'.'hdmi5'.'True'.'Include'.'hdmi5'.'If'.'hdmi'.'Condition' - True": 1,
   "HDA-Intel/HDA-Intel.conf@HiFi.conf: 'If'.'hdmi'.'True'.'Include'.'hdmi'.'If'.'hdmi6'.'True'.'Include'.'hdmi5'.'If'.'hdmi'.'Condition' - True": 1,
